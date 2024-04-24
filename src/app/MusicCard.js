b2dc465240a73614eb07f0981f1051fbc2731af4
abhis12321 <abhisheksingh2359328@gmail.com>
2024-04-24 14:42:21 +0530

    backward and forward button enabled

--- src/app/MusicCard.js
+++ src/app/MusicCard.js
@@ -4,42 +4,78 @@ import Image from 'next/image';
 import React from 'react'
 
 export default function MusicCard(props) {
-    const playMusic = () => {
-        let cantainer = document.querySelector(`#${props.data.name}`);
-        let audio = cantainer.querySelector('audio');
-        let musicAvatar = cantainer.querySelector('p');
-        if(audio.paused) {
-            musicAvatar.classList.toggle('animate-spin')
-            audio.play();
-        } else {
-            musicAvatar.classList.toggle('animate-spin')
-            audio.pause();
-        }
-    }
-
-    const handleMusic = e => {
-        let svgs = e.target.parentElement.querySelectorAll('svg');
+    const handleButton = (cantainer) => {        
+        let group = cantainer.querySelector('.group');
+        let svgs = group.querySelectorAll('svg');
         svgs[0]?.classList?.toggle('block');
         svgs[0]?.classList?.toggle('hidden');
         svgs[1]?.classList?.toggle('block');
         svgs[1]?.classList?.toggle('hidden');
-        playMusic();
     }
+
+    const playMusic = (cantainer) => {
+        let audio = cantainer.querySelector('audio');
+        let musicAvatar = cantainer.querySelector('p');
+        if(audio.paused) {
+            musicAvatar.classList.toggle('animate-spin');
+            audio.play();
+        } else {
+            musicAvatar.classList.toggle('animate-spin');
+            audio.pause();
+        }
+    }
+
+    const handleMusic = () => {
+        let cantainer = document.querySelector(`#${props.data.name}`);
+        handleButton(cantainer);
+        playMusic(cantainer);
+    }
+
+    const pause = (cantainer) => {
+        let musicAvatar = cantainer.querySelector('p');
+        let audio = cantainer.querySelector('audio');
+        if(!audio.paused) {
+            musicAvatar.classList.remove('animate-spin');
+            audio.pause();
+            handleButton(cantainer);
+        }
+    }
+
+    const nextPlay = () => {
+        let cantainer = document.querySelector(`#${props.data.name}`);
+        let nextCant = cantainer.nextElementSibling;
+        if(nextCant) {
+            pause(cantainer);            
+            handleButton(nextCant);
+            playMusic(nextCant);
+        }
+    }
+
+    const previousPlay = () => {
+        let cantainer = document.querySelector(`#${props.data.name}`);
+        let previousCant = cantainer.previousElementSibling;
+        if (previousCant) {
+          pause(cantainer);
+          handleButton(previousCant);
+          playMusic(previousCant);
+        }
+      }
+    
     return (
-        <div id={props.data.name} className='flex flex-col items-center justify-evenly pb-3 bg-slate-900 h-[400px] w-[300px] mx-auto rounded-md shadow-[0_0_8px_white] hover:shadow-[0_0_12px_red]'>
+        <div id={props.data.name} className='flex flex-col items-center justify-evenly pb-3 bg-gradient-to-b from-slate-950 via-slate-700 to-gray-950 h-[400px] w-[300px] mx-auto rounded-md shadow-[0_0_8px_white] hover:shadow-[0_0_12px_red]'>
             <h2 className="my-4 font-bold text-2xl drop-shadow-[0_0_7px_violet] p-2 text-center">{props.data.name}</h2>
             <p className="w-[200px] h-[200px] [background-image:conic-gradient(red , yellow , green , blue , red)] bg-conic-gradient rounded-full border-4"></p>
             <div className=' text-yellow-500 font-semibold'>{props.data.singer}</div>
             <audio src={'/music/' + props.data.name + '.mp3'} className=''></audio>
 
             <div className="flex items-center justify-around w-full">
-                <FontAwesomeIcon icon={faBackward} className='text-xl cursor-pointer hover:text-gray-500 w-6' size='' />
+                <FontAwesomeIcon icon={faBackward} className='text-xl cursor-pointer hover:text-gray-500 w-6' size='' onClick={previousPlay} />
                 <div className="group w-fit relative">
-                    <FontAwesomeIcon icon={faPlay} className='w-8 h-8 cursor-pointer group-hover:text-gray-500 bg-gray-950 rounded-full py-[10px] pl-[11px] pr-[8px] block group-active:hidden' size='' />
-                    <FontAwesomeIcon icon={faPause} className='w-8 h-8 cursor-pointer group-hover:text-gray-500 bg-gray-950 rounded-full p-[10px] hidden group-active:block' size='' />
+                    <FontAwesomeIcon icon={faPlay} className='w-8 h-8 cursor-pointer group-hover:text-gray-500 bg-gray-950 rounded-full py-[10px] pl-[11px] pr-[8px] block group-active:hidden' size='2x' />
+                    <FontAwesomeIcon icon={faPause} className='w-8 h-8 cursor-pointer group-hover:text-gray-500 bg-gray-950 rounded-full p-[10px] hidden group-active:block' size='2x' />
                     <div className="absolute w-14 h-14 z-10 top-0 left-0 cursor-pointer rounded-full" onClick={handleMusic}></div>
                 </div>
-                <FontAwesomeIcon icon={faForward} className='text-xl cursor-pointer hover:text-gray-500 w-6' size='' />
+                <FontAwesomeIcon icon={faForward} className='text-xl cursor-pointer hover:text-gray-500 w-6' size='' onClick={nextPlay} />
             </div>
         </div>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -1,96 +1,153 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import MusicCard from './MusicCard'
 
-export default function page() {
-  const songs = [
-    {
-      name:'Ae-gujarne-waali-hwa',
-      singer:"Rahul Gandhi"
-    },
-    {
-      name:"All-is-well",
-      singer:"Rahul Gandhi"
-    },
-    {
-      name:"Badan-pe-sitare-lapete-hue",
-      singer:"Rahul Gandhi"
-    },
-    {
-      name:"Bande-hai-hm-uske",
-      singer:"Rahul Gandhi"
-    },
-    {
-      name:"Behti-hwa-sa-tha-wo",
-      singer:"Rahul Gandhi"
-    },
-    {
-      name:"Bella-ciao",
-      singer:"Rahul Gandhi"
-    },
-    {
-      name:"Desh-mere",
-      singer:"Rahul Gandhi"
-    },
-    {
-      name:"Give-me-some",
-      singer:"Rahul Gandhi"
-    },
-    {
-      name:"Gori-tori-chunari",
-      singer:"Rahul Gandhi"
-    },
-    {
-      name:"Haal-kya-hai-dilon-ka",
-      singer:"Rahul Gandhi"
-    },
-    {
-      name:"Hai-zindgi-kitni-khubsurat",
-      singer:"Rahul Gandhi"
-    },
-    {
-      name:"Hey-ram",
-      singer:"Rahul Gandhi"
-    },
-    {
-      name:"Kamata-hum-bahut-kuchh-pr",
-      singer:"Rahul Gandhi"
-    },
-    {
-      name:"Kehdun-tumhe-ya-chup-rhun",
-      singer:"Rahul Gandhi"
-    },
-    {
-      name:"Log-kehte-hain-mai-sarabi-hun",
-      singer:"Rahul Gandhi"
-    },
-    {
-      name:"Dhire-dhire-meri-jindgi-me-aana",
-      singer:"Rahul Gandhi"
-    },
-    {
-      name:"Mere-umar-k-nav-jawaano",
-      singer:"Rahul Gandhi"
-    },
-    {
-      name:"Peele-peele-o-mere-raaja",
-      singer:"Rahul Gandhi"
-    },
-    {
-      name:"Tu-cheez-bdi-hai-mast",
-      singer:"Rahul Gandhi"
-    },
-    {
-      name:"Yaad-aa-rhi-hai",
-      singer:"Rahul Gandhi"
-    },
-  ]
+const songs = [
+  {
+    name: 'Ae-gujarne-waali-hwa',
+    singer: "Rahul Gandhi"
+  },
+  {
+    name: "All-is-well",
+    singer: "Rahul Gandhi"
+  },
+  {
+    name: "Badan-pe-sitare-lapete-hue",
+    singer: "Rahul Gandhi"
+  },
+  {
+    name: "Bande-hai-hm-uske",
+    singer: "Rahul Gandhi"
+  },
+  {
+    name: "Behti-hwa-sa-tha-wo",
+    singer: "Rahul Gandhi"
+  },
+  {
+    name: "Bella-ciao",
+    singer: "Rahul Gandhi"
+  },
+  {
+    name: "Bella-ciao-hindi",
+    singer: "Rahul Gandhi"
+  },
+  {
+    name: "Desh-mere",
+    singer: "Rahul Gandhi"
+  },
+  {
+    name: "Give-me-some",
+    singer: "Rahul Gandhi"
+  },
+  {
+    name: "Gori-tori-chunari",
+    singer: "Rahul Gandhi"
+  },
+  {
+    name: "Haal-kya-hai-dilon-ka",
+    singer: "Rahul Gandhi"
+  },
+  {
+    name: "Hai-zindgi-kitni-khubsurat",
+    singer: "Rahul Gandhi"
+  },
+  {
+    name: "Hey-ram",
+    singer: "Rahul Gandhi"
+  },
+  {
+    name: "Kamata-hum-bahut-kuchh-pr",
+    singer: "Rahul Gandhi"
+  },
+  {
+    name: "Kehdun-tumhe-ya-chup-rhun",
+    singer: "Rahul Gandhi"
+  },
+  {
+    name: "Log-kehte-hain-mai-sarabi-hun",
+    singer: "Rahul Gandhi"
+  },
+  {
+    name: "Dhire-dhire-meri-jindgi-me-aana",
+    singer: "Rahul Gandhi"
+  },
+  {
+    name: "Mere-umar-k-nav-jawaano",
+    singer: "Rahul Gandhi"
+  },
+  {
+    name: "Peele-peele-o-mere-raaja",
+    singer: "Rahul Gandhi"
+  },
+  {
+    name: "Tu-cheez-bdi-hai-mast",
+    singer: "Rahul Gandhi"
+  },
+  {
+    name: "Yaad-aa-rhi-hai",
+    singer: "Rahul Gandhi"
+  },
+]
+
+export default function Page() {
+  const [player , setPlayer] = useState();
+
+  const toggleButton = (cantainer) => {
+    let group = cantainer.querySelector('.group');
+    let svgs = group.querySelectorAll('svg');
+    svgs[0]?.classList?.toggle('block');
+    svgs[0]?.classList?.toggle('hidden');
+    svgs[1]?.classList?.toggle('block');
+    svgs[1]?.classList?.toggle('hidden');
+  }
+
+  const handleMusic = (cantainer) => {
+    let audio = cantainer.querySelector('audio');
+    let musicAvatar = cantainer.querySelector('p');
+    if (audio.paused) {
+      musicAvatar.classList.toggle('animate-spin');
+      audio.play();
+    } else {
+      musicAvatar.classList.toggle('animate-spin');
+      audio.pause();
+    }
+  }
+
+  const pause = (cantainer) => {
+    let musicAvatar = cantainer.querySelector('p');
+    let audio = cantainer.querySelector('audio');
+    if (!audio.paused) {
+      musicAvatar.classList.remove('animate-spin');
+      audio.pause();
+      handleButton(cantainer);
+    }
+  }
+
+  const nextPlay = (cantainer) => {
+    let nextCant = cantainer.nextElementSibling;
+    if (nextCant) {
+      pause(cantainer);
+      handleButton(nextCant);
+      playMusic(nextCant);
+    }
+  }
+
+  const previousPlay = () => {
+    let cantainer = document.querySelector(`#${props.data.name}`);
+    let previousCant = cantainer.previousElementSibling;
+    if (previousCant) {
+      pause(cantainer);
+      handleButton(previousCant);
+      playMusic(previousCant);
+    }
+  }
+
   return (
-    <div className='flex flex-wrap gap-8 items-center justify-center p-4' style={{minHeight:'calc(100vh - 4rem)'}}>
+    <div className='flex flex-wrap gap-8 items-center justify-center p-4' style={{ minHeight: 'calc(100vh - 4rem)' }}>
       {
-      songs?.map((song , index) => 
-        <MusicCard data={song} key={index}/>
-      )}
+        songs?.map((song, index) =>
+          <MusicCard data={song} functions = {{toggleButton , handleMusic , pause , nextPlay , previousPlay}} key={index} />
+        )}
     </div>
   )
 }
